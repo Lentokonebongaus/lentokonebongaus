c5fd8ac0c7fceea9a463633a46ee0edb4ae0c5fa
Eelii <61108072+Eelii@users.noreply.github.com>
2022-02-03 16:56:54 +0200

    Initial RegisterView push

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RegisterView from './RegisterView';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <RegisterView/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
