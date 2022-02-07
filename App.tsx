360988824d2dd2fb9225692997e132a19c78cd7c
Eeliii <eeli.killstrom@myy.haaga-helia.fi>
2022-02-07 16:50:30 +0200

    Connected Firebase to RegisterView

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import RegisterView from './RegisterView';
+import{ initializeApp } from "firebase/app";
+import { getDatabase, push, ref, onValue, update } from 'firebase/database';
+import { useEffect, useState } from 'react';
+
+
 export default function App() {
+
   return (
     <View style={styles.container}>
+      <Text></Text>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
