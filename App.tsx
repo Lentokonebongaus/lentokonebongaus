213f2f8167f9556f87b6a40427545f0dee8e55c6
Iida Rokka <iida.rokka@hotmail.fi>
2022-02-08 11:23:34 +0200

    Kotinakyma update ja Tab navigaatio kotisivulle

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import RegisterView from './RegisterView';
 import{ initializeApp } from "firebase/app";
 import { getDatabase, push, ref, onValue, update } from 'firebase/database';
 import { useEffect, useState } from 'react';
+import Kotinakyma from "./Kotinakyma";
+import { NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
+import { Ionicons} from '@expo/vector-icons';  
+import Map from "./Map"
 
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  )
+    <NavigationContainer>
+      <Tab.Navigator
+      screenOptions={({route }) => ({ 
+      tabBarIcon: ({ focused, color, size }) => {             
+             
+        let iconName;            
+        if (route.name === 'Home') {             
+           iconName = 'md-home';            
+          } else if (route.name === 'Map') {
+            iconName = 'md-map';            
+          }            
+          // näyttää punaista mutta toimii
+          return <Ionicons name={iconName}size={size}color={color} />;   
+            
+        },        
+        })}>
+        <Tab.Screen name="Home" component={Kotinakyma} />
+        <Tab.Screen name="Map" component={Map} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
