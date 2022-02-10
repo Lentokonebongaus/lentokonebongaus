5db0dfa2345deab4c8f0cd361aca6251df11ae6c
Eeliii <eeli.killstrom@myy.haaga-helia.fi>
2022-02-10 23:16:43 +0200

    Showing user location on Android

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,44 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 import {PROVIDER_GOOGLE} from 'react-native-maps';
-
+// Hakee ainakin Androidilla sijainnin expo-location -kirjaston avulla. Kannattaa vielä testata, että toimii myös iOS:llä. -Eeli 
+import * as Location from 'expo-location';
 
 
 export default function Map() {
+
+  // Because MapView is rendered before user location is set, region state is used to set the initial map view to a pre-determined location.
+  // A short loading screen would maybe be another, more adequate solution? -Eeli
+  const [location, setLocation] = useState({longitude:0, latitude:0});
+  const [region, setRegion] = useState({longitude:24.9049634, latitude:60.2494251 , latitudeDelta: 0.20, longitudeDelta: 0.02});
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={{height: '100%', width: '100%'}} provider={PROVIDER_GOOGLE} showsUserLocation={true} showsCompass={true} showsScale={true} showsTraffic={true} />
+      <MapView 
+        style={{height: '100%', width: '100%'}} 
+        provider={PROVIDER_GOOGLE} 
+        showsUserLocation={true} 
+        showsCompass={true} 
+        showsScale={true} 
+        showsTraffic={true}
+        initialRegion={region}
+      />
     </View>
   );
 }
