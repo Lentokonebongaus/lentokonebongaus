b6b757674cbbf8a24da7be69f3628f318844e41c
Eeliii <eeli.killstrom@myy.haaga-helia.fi>
2022-03-11 19:11:55 +0200

    Displaying a circle around user location

---
+++ Card.tsx
@@ -0,0 +1,13 @@
+class Card {
+    cardId: number
+    cardQuality: number
+    planePicture: string
+    planeModel: string
+    planeSize: string
+
+    constructor(cardData){
+        // TODO
+    }
+}
+
+export default Card
