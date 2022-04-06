ed1097aefb06a6797fd305904318a21c229a6605
Eeli Killström <eeli.killstrom@gmail.com>
2022-04-06 23:31:29 +0300

    Plane cards now with even more data!

--- util/planeDetails.tsx
+++ util/planeDetails.tsx
@@ -1,6 +1,6 @@
 async function fetchPlaneDetails(icao24:String){
-    const backendUrl = "http://192.168.1.7:5000"
-    const planePromise = await fetch(`http://192.168.1.7:5000?icao24=${icao24}`)
+    const backendUrl = "http://192.168.1.143:5000"
+    const planePromise = await fetch(`${backendUrl}?icao24=${icao24}`)
     const planeDetails = await planePromise.json()
     return planeDetails
 }
