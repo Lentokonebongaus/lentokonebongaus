001cf87bcee2700f8cd61153621755836148037f
Iida Rokka <iida.rokka@hotmail.fi>
2022-04-13 04:01:48 +0300

    single card view with draggable card, changing star colors

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,11 +1,7 @@
 import { Text, View, FlatList, Image, TouchableOpacity} from "react-native";
 import testCard from '../testCard';
-import star from "../assets/star4.png";
-import star2 from "../assets/2star.png";
-import star3 from "../assets/3star.png";
-import star4 from "../assets/4star.png";
-import star5 from "../assets/5star.png";
 import { styles } from '../util/styles';
+import { AntDesign } from '@expo/vector-icons'; 
 
 type Props = {
     navigation: any
@@ -33,16 +29,28 @@ export default function Cards(Props: Props){
                 {item.planePicture != ""?  <Image
                 source={{uri: item.planePicture}}/>: null }
 
-                {item.cardQuality < 5?  <Image
-                source={star5}/>: null }
-                {item.cardQuality < 100 && item.cardQuality > 5? <Image
-                source={star4}/>: null }
-                {item.cardQuality < 500 && item.cardQuality > 100? <Image
-                source={star3}/>: null }
-                {item.cardQuality < 1000 && item.cardQuality > 500? <Image
-                source={star2}/>: null }
-                {item.cardQuality > 1000? <Image
-                source={star}/>: null }
+                {item.cardQuality < 5? 
+                <Text>
+                     {Array.from({ length: 5 }, (_, i) => 
+                    <AntDesign name="star" size={24} color="gold" />)}
+                </Text>: null }
+                {item.cardQuality < 100 && item.cardQuality > 5? 
+                <Text>
+                     {Array.from({ length: 4 }, (_, i) => 
+                    <AntDesign name="star" size={24} color="orange" />)}
+                </Text>: null }
+                {item.cardQuality < 500 && item.cardQuality > 100? <Text>
+                    {Array.from({ length: 3 }, (_, i) => 
+                    <AntDesign name="star" size={24} color="#c4c4c4" />)}
+                </Text>: null }
+                {item.cardQuality < 1000 && item.cardQuality > 500? 
+                <Text>
+                 {Array.from({ length: 2 }, (_, i) => 
+                    <AntDesign name="star" size={24} color="#c41c10" />)}
+                </Text>
+                : null }
+                {item.cardQuality > 1000? 
+                <AntDesign name="star" size={24} color="white" />: null }
 
                 <Text style={styles.cardText}>Manufacturer: {item.planeManufacturer}</Text>
                 <Text style={styles.cardText}>Model: {item.planeModel}</Text>
