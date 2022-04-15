3eea173be8a536b4967253774b8431e26d3ac9de
Eeli Killström <eeli.killstrom@gmail.com>
2022-04-15 16:37:51 +0300

    Changed backend address

--- util/planeDetails.tsx
+++ util/planeDetails.tsx
@@ -1,5 +1,5 @@
 async function fetchPlaneDetails(icao24:String){
-    const backendUrl = "http://192.168.1.143:5000"
+    const backendUrl = "http://172.105.80.249"
     const planePromise = await fetch(`${backendUrl}?icao24=${icao24}`)
     const planeDetails = await planePromise.json()
     return planeDetails
