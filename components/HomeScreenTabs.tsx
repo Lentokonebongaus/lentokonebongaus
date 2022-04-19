9d927193b494f9a01a98c9b638ae54afb4a0a091
Iida Rokka <iida.rokka@hotmail.fi>
2022-04-20 02:04:47 +0300

    Kotinäkymän ja tab navin tyylitys

--- components/HomeScreenTabs.tsx
+++ components/HomeScreenTabs.tsx
@@ -11,7 +11,7 @@ export default function HomeScreenTabs(){
     return (
         <Tab.Navigator
         screenOptions={({route }) => ({ 
-        tabBarIcon: ({ focused, color, size }) => {             
+        tabBarIcon: ({ focused, color, size}) => {             
                 
             let iconName;            
             if (route.name === 'Home') {             
@@ -21,9 +21,15 @@ export default function HomeScreenTabs(){
             } else if (route.name === 'List')
                 iconName = 'md-list'
             // näyttää punaista mutta toimii
-            return <Ionicons name={iconName}size={size}color={color} />;   
+            return <Ionicons name={iconName}size={size}color={color} />;  
+             
                 
         },
+        tabBarStyle: {
+            backgroundColor: "rgba(25, 58, 165, 1)"
+        },
+        tabBarActiveTintColor: "#9af5e7", 
+        tabBarInactiveTintColor: "white",
         headerShown: false         
         })}>
             <Tab.Screen name="Home" component={Kotinakyma} />
