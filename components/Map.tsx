971ebcc726009ec87d3c94787b14edad9a58b021
Eeli Killström <eeli.killstrom@gmail.com>
2022-05-06 14:51:26 +0300

    Added possibilty to upgrade existing card in planeView and different icons for upgradable planes in mapView

--- components/Map.tsx
+++ components/Map.tsx
@@ -13,6 +13,7 @@ import planeIcon from '../assets/plane_icon.png'
 import planeIconGrounded from '../assets/plane_icon_grounded.png'
 import planeIconGroundedCollected from '../assets/plane_icon_grounded_collected.png'
 import planeIconCollected from '../assets/plane_icon_collected.png'
+import planeIconCollectedUpgradable from '../assets/plane_icon_collected_upgradable.png'
 import { cardsDb } from '../util/Firebase';
 import { refreshPlanes, setGPSlocation } from '../util/locationFunctions';
 import { getDatabase, push, ref, onValue, update, get } from 'firebase/database';
@@ -20,6 +21,7 @@ import { LoggedUsernameContext } from '../util/LoggedUsernameProvider';
 import { UserCardsContext, updateUserCardsContext } from '../util/UserCardsProvider';
 import { PlanesContext } from '../util/PlanesProvider';
 import { styles } from '../util/styles';
+import Card from '../util/Card';
 import { UserLocationContext, refreshUserLocationContext } from '../util/UserLocationProvider';
 
 
@@ -69,12 +71,17 @@ export default function Map(props:any) {
   const getPlaneIcon = (plane:Plane) => {
 
     const planeInUserCards = icao24NotInUsersCards(plane.icao24)
+    const tmpNewCard = new Card(plane, loggedUsername, "tmpUrl")
     if(plane.onGround && !planeInUserCards){
       return planeIconGrounded
     } else if (plane.onGround && planeInUserCards){
       return planeIconGroundedCollected
     } else if (!plane.onGround && planeInUserCards){
-      return planeIconCollected
+      if(getUserCardByIcao24(plane.icao24).cardQuality < tmpNewCard.cardQuality){
+        return planeIconCollectedUpgradable
+      } else{
+        return planeIconCollected
+      }
     } else {
       return planeIcon
     }
@@ -86,6 +93,17 @@ export default function Map(props:any) {
       for (let i = 0; i < cardIds.length; i++){
         if(userCards[cardIds[i]].planeIcao24 == icao24){
           return true
+        }
+      }
+      return false
+  }
+
+  function getUserCardByIcao24(icao24:String){
+
+    const cardIds = Object.keys(userCards)
+      for (let i = 0; i < cardIds.length; i++){
+        if(userCards[cardIds[i]].planeIcao24 == icao24){
+          return userCards[cardIds[i]]
         }
       }
       return false
@@ -115,7 +133,7 @@ export default function Map(props:any) {
                   }}
                   title={plane.icao24}
                   onPress={()=>{props.navigation.navigate("Plane", {plane:plane, location:userLocation})}}
-                  // plane_icon.png isn't currently aligned with Plane object's trueTrack attribute, so even though trueTrack is measured in degrees
+                  // plane_icon.png isn't currently aligned with Plane object's trueTrack attribute, so even though trueTrack is  measured in degrees
                   // similar to Marker component's rotation prop, png file's unalignment needs to be taken into account. 
                   rotation={plane.trueTrack+50}
                 />
@@ -125,7 +143,7 @@ export default function Map(props:any) {
         }
         <Circle 
           center={{latitude:userLocation.latitude, longitude:userLocation.longitude}} 
-          radius={47000}
+          radius={50000}
           options={{
             strokeColor: "#FF0000",
             strokeOpacity: 0.8,
